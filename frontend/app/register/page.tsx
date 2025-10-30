177ac3b79e1f4c5cbdbd815d79309f3466699069
justcodeon231 <ntlukaleo@gmail.com>
2025-10-30 10:33:31 +0200

    DELETED FRONTEND CODE

--- frontend/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "@/components/register-form"
-
-export default function RegisterPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-background">
-      <RegisterForm />
-    </div>
-  )
-}
